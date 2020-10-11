5f62eb24466360a0b1ee90d49046bf7d54694004
melodeenae5 <danaekirby5@gmail.com>
2020-10-10 19:49:10 -0500

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const theSequence = [0, 1, 2];
+const theSequence = [0, 1, 2, 2, 3, 3, 0, 3, 2, 1, 3];
 const userSequence = [];
 const modal = document.getElementById('myModal');
 const playButton = document.querySelector('.play');
@@ -16,8 +16,18 @@ function randomNumber() {
 }
 
 function playGame() {
-	for (let i = 0; i < theSequence.length; i++) {
+	let i = 0;
+	const flash = setInterval(function () {
 		const square = document.getElementById(theSequence[i]);
-		square.classList.toggle(`flash${theSequence[i]}`);
-	}
+		const element = square.cloneNode(true);
+		square.classList.add(`flash${theSequence[i]}`);
+		setTimeout(function () {
+			square.classList.remove(`flash${theSequence[i]}`);
+			square.parentNode.replaceChild(element, square);
+		}, 1000);
+		i++;
+		if (i >= theSequence.length) {
+			clearInterval(flash);
+		}
+	}, 1500);
 }
